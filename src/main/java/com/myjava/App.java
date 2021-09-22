d3cf62b5cd2775e69def83c15adab9ad7bfd6166
SHAILESH <shaileshdubeyr@gmail.com>
2021-09-22 09:07:30 +0530

    added : hello word with log4j

---
+++ src/main/java/com/myjava/App.java
@@ -0,0 +1,27 @@
+package com.myjava;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.myjava.App;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    		final Logger Log= LogManager.getLogger(App.class);
+        	String message = "Hello  world";
+        	Log.debug(message + "will be printed on debug" );
+        	Log.info(message +"will be printed on debug" );
+        	Log.warn(message + "will be printed on debug" );
+        	Log.error(message + "will be printed on debug" );
+        	Log.fatal(message + "will be printed on debug" );
+        	Log.info("Appending string : {}.",message);
+            System.out.println(message);
+            System.out.println( "Hello World!" );
+    }
+}
