e80e1e306524ed42c235e7c452de10936e1aca5f
SHAILESH <shaileshdubeyr@gmail.com>
2021-09-25 02:47:37 +0530

    Added : Code for employee is present for half and full day

--- EmployeeAttendence.java
+++ EmployeeAttendence.java
@@ -3,21 +3,28 @@ package com.myjava;
 public class EmployeeAttendence {
 	
 	public static void main(String[] args) {
-			
-			int WagPerHrs = 20;
-			int FullDay = 8;
-			int TotalSal;
+		
 			System.out.println("Welcome to Employee Wage Computation");
-			int EmpStatus=(int)Math.random() * 2;
-			if(EmpStatus == 1)
+			int WagPerHrs = 20;
+			int FullTime = 8;
+			int PartTime = 4;
+			int TotalSal;
+			int Isfor = (int)(Math.random() * 3);
+			if(Isfor == 1)
 			{
-				System.out.println("Employee is present");
-				TotalSal=WagPerHrs * FullDay *EmpStatus;
+				System.out.println("Employee is present for full time");
+				TotalSal=WagPerHrs * FullTime;
+			}
+			else if (Isfor == 2)
+			{
+				System.out.println("Employee is present for part time");
+				TotalSal= WagPerHrs * PartTime;
+				
 			}
 			else
 			{
 				System.out.println("Employee is not present");
-				TotalSal= WagPerHrs * FullDay *EmpStatus;
+				TotalSal= WagPerHrs * FullTime * Isfor;
 			}
 		System.out.println("The total salary is " +TotalSal);
 	}
