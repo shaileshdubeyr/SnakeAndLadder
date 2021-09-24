bc38fdeb3a114df0824b0896af53139416f5a51e
SHAILESH <shaileshdubeyr@gmail.com>
2021-09-25 01:03:45 +0530

    Added : Employee ststus

--- EmployeeAttendence.java
+++ EmployeeAttendence.java
@@ -5,6 +5,16 @@ public class EmployeeAttendence {
 	public static void main(String[] args) {
 
 			System.out.println("Welcome to Employee Wage Computation");
+			int EmpStatus = (int)Math.random() * 2;
+			System.out.println(EmpStatus);
+			if(EmpStatus == 1)
+			{
+				System.out.println("Employee is present");
+			}
+			else
+			{
+				System.out.println("Employee is not present");
+			}
 	}
 
 }
