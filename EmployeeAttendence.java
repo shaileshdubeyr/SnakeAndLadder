631d129379f4210b968505deb2e1022140f4147c
SHAILESH <shaileshdubeyr@gmail.com>
2021-09-25 01:53:24 +0530

    A

--- EmployeeAttendence.java
+++ EmployeeAttendence.java
@@ -3,18 +3,23 @@ package com.myjava;
 public class EmployeeAttendence {
 	
 	public static void main(String[] args) {
-
+			
+			int WagPerHrs = 20;
+			int FullDay = 8;
+			int TotalSal;
 			System.out.println("Welcome to Employee Wage Computation");
-			int EmpStatus = (int)Math.random() * 2;
-			System.out.println(EmpStatus);
+			int EmpStatus=(int)Math.random() * 2;
 			if(EmpStatus == 1)
 			{
 				System.out.println("Employee is present");
+				TotalSal=WagPerHrs * FullDay * EmpStatus;
 			}
 			else
 			{
 				System.out.println("Employee is not present");
+				TotalSal= WagPerHrs * FullDay *EmpStatus;
 			}
+		System.out.println("The total salary is " +TotalSal);
 	}
 
 }
