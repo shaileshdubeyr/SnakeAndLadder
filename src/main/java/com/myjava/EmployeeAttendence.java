f668d1e39a94cca26efdff765e19aeaf46c96d3d
SHAILESH <shaileshdubeyr@gmail.com>
2021-09-24 16:21:12 +0530

    Adding : Employee present  or absent program

---
+++ src/main/java/com/myjava/EmployeeAttendence.java
@@ -0,0 +1,19 @@
+package com.myjava;
+
+public class EmployeeAttendence {
+	
+	public static void main(String[] args) {
+
+			int num=(int)Math.random()*2;
+			System.out.println(num);
+			if(num==1)
+			{
+				System.out.println("employee is present");
+			}
+			else
+			{
+				System.out.println("employee is not present");
+			}
+	}
+
+}
