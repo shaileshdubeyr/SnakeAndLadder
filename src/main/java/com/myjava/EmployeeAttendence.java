555cf5a047c51407ab1f9e6858c0e1f25f618ae1
SHAILESH <shaileshdubeyr@gmail.com>
2021-09-24 19:13:43 +0530

    Added : UC Welcome message

--- src/main/java/com/myjava/EmployeeAttendence.java
+++ src/main/java/com/myjava/EmployeeAttendence.java
@@ -4,16 +4,7 @@ public class EmployeeAttendence {
 	
 	public static void main(String[] args) {
 
-			int num=(int)Math.random()*2;
-			System.out.println(num);
-			if(num==1)
-			{
-				System.out.println("employee is present");
-			}
-			else
-			{
-				System.out.println("employee is  nn not present");
-			}
+			System.out.println("Welcome to Employee Wage Computation");
 	}
 
 }
