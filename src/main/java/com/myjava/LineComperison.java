15cb014d67decbbd8f01ec439b81949e01ef00f1
SHAILESH <shaileshdubeyr@gmail.com>
2021-09-27 02:18:02 +0530

    Added : Code for line equal or not

--- src/main/java/com/myjava/LineComperison.java
+++ src/main/java/com/myjava/LineComperison.java
@@ -1,5 +1,7 @@
 package com.myjava;
 
+import org.apache.logging.log4j.core.pattern.EqualsBaseReplacementConverter;
+
 public class LineComperison {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the line compersion computation");
@@ -7,9 +9,20 @@ public class LineComperison {
 		int x2 = (int)(Math.random() * 9);
 		int y1 = (int)(Math.random() * 9);
 		int y2 = (int)(Math.random() * 9);
+		int x3 = (int)(Math.random() * 9);
+		int x4 = (int)(Math.random() * 9);
+		int y3 = (int)(Math.random() * 9);
+		int y4 = (int)(Math.random() * 9);
 		System.out.printf("the x1 %d and x2 %d and y1 %d y2 %d \n",x1,x2,y1,y2);
-		double length = (float)Math.sqrt( ( (x2-x1) ^2 ) + ( (y2-y1) ^2) );
-		System.out.printf("the length is %.2f \n",length);
+		System.out.printf("the x3 %d and x4 %d and y3 %d y4 %d \n",x3,x4,y3,y4);
+		double slope = y2-y1 / x2-x1;
+		System.out.printf("the slope is line 1 %.2f \n",slope);
+		double slope1 = y4-y3 / x4-x3;
+		System.out.printf("the slope is line 2 %.2f \n",slope1);
+		String string= String.valueOf(slope);
+		String string1=String.valueOf(slope1);
+		System.out.println(string.equals(string1));
+		
 	}
 
 }
