30a9d2492e4c51ec4697126e6a26bef462c5c667
SHAILESH <shaileshdubeyr@gmail.com>
2021-09-27 09:21:53 +0530

    Added : code for dice value

--- src/main/java/com/myjava/SnakeAndLadder.java
+++ src/main/java/com/myjava/SnakeAndLadder.java
@@ -3,11 +3,11 @@ package com.myjava;
 public class SnakeAndLadder {
 	public static void main(String[] args) {
 		int playerPositation;
-		int diceValue = (int)(Math.random() *7 );
-		int playerStatus = (int)( ( Math.random() * 2 ) + 1 );
+		int diceValue = (int)( (Math.random() *6 ) + 1 );
+		int playerStatus = (int)(  Math.random() * 2 );
 		if(playerStatus == 1 ) {
 			playerPositation = 0;
-			System.out.println("the player is playing  positation is "+playerPositation);
+			System.out.println(" the player is playing  positation is "+playerPositation);
 		}
 		else
 			System.out.println("the player is not playing");
