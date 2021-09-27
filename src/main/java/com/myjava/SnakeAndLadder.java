c8997c852df7c4a6f39874d5b4350ae2951904f0
SHAILESH <shaileshdubeyr@gmail.com>
2021-09-27 20:04:04 +0530

    Added : Code for sanke ladder nad default positation

--- src/main/java/com/myjava/SnakeAndLadder.java
+++ src/main/java/com/myjava/SnakeAndLadder.java
@@ -1,17 +1,30 @@
 package com.myjava;
 
 public class SnakeAndLadder {
+	
 	public static void main(String[] args) {
-		int playerPositation;
-		int diceValue = (int)( (Math.random() *6 ) + 1 );
-		int playerStatus = (int)(  Math.random() * 2 );
-		if(playerStatus == 1 ) {
-			playerPositation = 0;
-			System.out.println(" the player is playing  positation is "+playerPositation);
-		}
-		else
-			System.out.println("the player is not playing");
-		
-		playerPositation = diceValue;
+			int positionOfplyer = playerPlayingLadderAndSnake();
+			System.out.println("the player positation is " +positionOfplyer);
 	}
+	static int playerPlayingLadderAndSnake() {
+		int playerPositatoion=0;
+		int ladderOrSnakeValue= (int)(Math.random() * 3 );
+		System.out.println("the snake ladder "+ladderOrSnakeValue);
+		int dieValue= (int)( (Math.random() * 6 ) + 1 );
+		System.out.println("the dice value is "+dieValue);
+			
+		if( ladderOrSnakeValue == 1 )
+			playerPositatoion= playerPositatoion + dieValue;		
+		else if( ladderOrSnakeValue == 2) {
+				playerPositatoion = playerPositatoion - dieValue;
+				if ( playerPositatoion < 0 )
+					playerPositatoion = 0;
+		}
+		else 
+			playerPositatoion = playerPositatoion;
+	
+		return playerPositatoion;
+	}
+		
 }
+
