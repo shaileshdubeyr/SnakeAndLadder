61aca2310834f8f15081e364e267ab033d25f8e3
SHAILESH <shaileshdubeyr@gmail.com>
2021-09-27 07:56:39 +0530

    Added : Code for dice rolling

--- src/main/java/com/myjava/SnakeAndLadder.java
+++ src/main/java/com/myjava/SnakeAndLadder.java
@@ -2,13 +2,16 @@ package com.myjava;
 
 public class SnakeAndLadder {
 	public static void main(String[] args) {
-		int playerStatus= (int)(Math.random() * 2 );
+		int playerPositation;
+		int diceValue = (int)(Math.random() *7);
+		int playerStatus = (int)(Math.random() * 2 );
 		if(playerStatus == 1 ) {
-			int playerPositation = 0;
+			playerPositation = 0;
 			System.out.println("the player is playing  positation is "+playerPositation);
 		}
 		else
-			System.out.println("the player is not playing");
+			System.out.println("the player is not playing" +diceValue);
+		
+		playerPositation = diceValue;
 	}
-
 }
