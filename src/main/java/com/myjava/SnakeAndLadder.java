58db5931568909f8a4a3915cc9f8658b65ef47e5
SHAILESH <shaileshdubeyr@gmail.com>
2021-09-28 00:47:15 +0530

    Added : Code for 2 player in snake and ladder

--- src/main/java/com/myjava/SnakeAndLadder.java
+++ src/main/java/com/myjava/SnakeAndLadder.java
@@ -3,39 +3,63 @@ package com.myjava;
 public class SnakeAndLadder {
 	
 	public static void main(String[] args) {
-			int positionOfplyer = playerPlayingLadderAndSnake();
-			System.out.println("the player positation is " +positionOfplyer);
+			playerPlayingLadderAndSnake();
 	}
-	static int playerPlayingLadderAndSnake() {
-		int playerPositatoion = 0;
-		int previousPositation;
-		int counter=0;
-		while(true) {
+	static void playerPlayingLadderAndSnake() {
+		int playerPositatoion1 = 0;
+		int previousPositation1;
+		int player2Positatoion = 0;
+		int previousPositationPlayer2;
+			while(true) {
 			int ladderOrSnakeValue = (int)(Math.random() * 3 );
 			System.out.println("the snake ladder "+ladderOrSnakeValue);
 			int dieValue = (int)( (Math.random() * 6 ) + 1 );
 			System.out.println("the dice value is "+dieValue);			
 			if ( ladderOrSnakeValue == 1 ) {
-				previousPositation = playerPositatoion; 
-				playerPositatoion= playerPositatoion + dieValue;
-				if ( playerPositatoion > 100 )
-					playerPositatoion = previousPositation;
+				previousPositation1 = playerPositatoion1; 
+				playerPositatoion1= playerPositatoion1 + dieValue;
+				if ( playerPositatoion1 > 100 )
+					playerPositatoion1 = previousPositation1;
+				continue;
 			}
 			else if ( ladderOrSnakeValue == 2) {
-				playerPositatoion = playerPositatoion - dieValue;
-				if ( playerPositatoion < 0 )
-					playerPositatoion = 0;
+				playerPositatoion1 = playerPositatoion1 - dieValue;
+				if ( playerPositatoion1 < 0 )
+					playerPositatoion1 = 0;
 			}
 			else 
-				playerPositatoion = playerPositatoion;
-				if ( playerPositatoion == 100 )
-					break;
-				
-		counter++;
-		System.out.println("the positation of the player "+playerPositatoion);
-		}
-			System.out.println("the total number of time die rolls "+counter);
-			return playerPositatoion;
+				playerPositatoion1 = playerPositatoion1;
+			if ( playerPositatoion1 == 100 ) {
+				System.out.println("the player1 win first");
+				break;
+			}
+playAgainPlayer2:if(true) {
+		        int ladderOrSnakeValue2 = (int)(Math.random() * 3 );
+		        System.out.println("the snake ladder "+ladderOrSnakeValue2);
+		        int dieValue2 = (int)( (Math.random() * 6 ) + 1 );
+		        System.out.println("the dice2 value is "+dieValue2);
+		        if ( ladderOrSnakeValue2 == 1 ) {
+		            previousPositationPlayer2 = player2Positatoion;
+		            player2Positatoion = player2Positatoion + dieValue2;
+		            if ( player2Positatoion > 100 )
+		                player2Positatoion = previousPositationPlayer2;
+		            break playAgainPlayer2;
+		        }
+		        else if ( ladderOrSnakeValue2 == 2) {
+		            player2Positatoion = player2Positatoion - dieValue2;
+		            if ( player2Positatoion < 0 )
+		                player2Positatoion = 0;
+		        }
+		        else
+		            player2Positatoion = player2Positatoion;
+		        if ( player2Positatoion == 100 ) {
+		        	System.out.println("player 2 win first");
+		         	break;
+		        }
+		    }
+		        
+		}	
+		//return playerPositatoion1;
 	}
 		
 }
